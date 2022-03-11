0c987c542757698fb48daba4349e043fab304496
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-10 16:23:39 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,11 +7,12 @@
  *
  *The string value is assigned to it
  * puts function is used to print out the string in str
- * Return: (0) Success 
+ * Return: (0) Success
  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);	
 	return (0);
 }	
