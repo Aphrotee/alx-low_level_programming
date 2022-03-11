4447b4a2c4052558a8eaff8185fd146dae9a7a7e
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-10 16:19:06 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,16 @@
 #include <string.h>
 
 /**
- * Entry point - main
- * The character type variable str is declared
- * The string value is assigned to it
+ * main - entry point
+ * @str The character type variable str is declared
+ *
+ *The string value is assigned to it
  * puts function is used to print out the string in str
- * Return (0)
+ * Return: (0) Success 
  */
 int main(void)
 {
-	char str[]="Programming is like building a multilingual puzzle\n";
-	
-	puts(str);
-	
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);	
 	return (0);
 }	
