ef1974b4558096725929c36e910f3390517be7ec
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-10 16:35:01 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * the string is printed with the printf function
+ * Rrturn: (0) Success
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art");
+	return (0);
+}
