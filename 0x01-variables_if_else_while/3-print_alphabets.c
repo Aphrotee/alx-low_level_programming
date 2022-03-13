d0c08019a4dc9881ec1116229c5c3d02e3fb2d63
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-03-13 22:51:50 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char alph;
+  char Alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+  Alph = 'A';
+	while (Alph <= 'Z')
+	{
+		putchar(Alph);
+		Alph++;
+  } 
+	putchar('\n');
+	return (0);
+}
