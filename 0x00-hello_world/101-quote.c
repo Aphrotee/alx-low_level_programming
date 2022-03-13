d5986d6bdde0d5b26ef051d8fdb77f26891c2303
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-13 09:37:34 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - entry point
- * @write: string
  *
  * the string in the writevariable is printed
- * Return: (1)
+ * Return: Always 1
  */
-int write(int filedes, const char *buf, unsingned int nbyte);
-int main(int argc, char** argv)
+int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
-	return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
