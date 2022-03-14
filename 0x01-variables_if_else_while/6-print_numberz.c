2f77cc8f7c492d5dde60a54f6c9587fb128e4303
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-14 05:33:52 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * maun - entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 9)
+	{
+		putchar('num');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
