2efdb3bbdfe8ff2bcd2d90327c327529a154eff3
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-16 08:06:45 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,17 @@
 #include <unistd.h>
-int _putchar(char c)
+int _putchar(char c);
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		if (alph = 'z')
+			_putchar('\n');
+		alph++;
+	}
+}
