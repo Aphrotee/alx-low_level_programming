a81e328b19eaff76d6eb6984e9a53fa64e8a717e
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-03-16 16:46:47 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,3 +10,16 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
+void print_alphabet(void)
+{
+	char alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
+}
