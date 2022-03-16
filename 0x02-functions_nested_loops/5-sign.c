60f56161cf2de6c105519503aa81849b5376795a
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-16 10:22:10 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,10 +11,19 @@ int print_sign(int n)
 	int i;
 
 	if (n > 0)
+	{
 		i = 1;
+		_putchar('+');
+	}
 	else if (n < 0)
+	{
 		i = -1;
+		_putchar('0');
+	}
 	else
+	{
 		i = 0;
+		_putchar('-');
+	}
 	return (i);
 }
