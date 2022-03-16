938c254732146e427afe692a3f670a1d7a0ae71d
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-16 09:28:43 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a-z ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alph;
+
+	for (i = 0; i <= 9; i++)
+	{
+		alph = 'a';
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+		putchar('\n');
+	}
+}
