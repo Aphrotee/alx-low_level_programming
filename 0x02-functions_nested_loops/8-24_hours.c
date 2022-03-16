dc4bbbb5556a3b8448f0d06eb862618ad75094e1
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-16 11:16:52 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the song
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d, e, f, g, h;
+
+	a = 0;
+	while (a < 2)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			c = 0;
+			while (c < 6)
+			{
+				d = 0;
+				while (d <= 9)
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	e = 2;
+	f = 0;
+	while (f < 4)
+	{
+		g = 0;
+		while (g < 6)
+		{
+			h = 0;
+			while (h <= 9)
+			{
+				_putchar(e + '0');
+				_putchar(f + '0');
+				_putchar(':');
+				_putchar(g + '0');
+				_putchar(h + '0');
+				h++;
+			}
+			g++;
+		}
+		f++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int k);
 int print_last_digit(int k);
+void jack_bauer(void);
