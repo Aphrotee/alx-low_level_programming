2c1bcd9f0c8e54996675bd2bbb50bf82f050f881
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-17 11:09:59 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 except 2 & 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if ((n != 2) && (n != 4))
+		{
+			_putchar(i + '0');
+			i++;
+		}
+		else
+			i++;
+	}
+	_putchar('\n');
+}
