8118c801ddb8aba9600d5cab6f5b2019b90406fa
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-17 11:22:37 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; J++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
