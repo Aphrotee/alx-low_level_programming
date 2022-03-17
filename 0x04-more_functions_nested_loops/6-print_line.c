73a43119efa500d00c228f997c9f8bc7fce923df
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-17 16:58:37 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints '_' n number of times to form a line
+ *@n: the length of the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
