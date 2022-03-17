8deaef685ad7a5797c660deae601c49f4b10ddc2
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-03-18 00:39:53 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,18 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			_putchar(j + '0');
+		}
+		for (k = 0; k <= 4; k++)
+		{
+			_putchar('1');
+			_putchar(k + '0');
 		}
 		_putchar('\n');
 	}
