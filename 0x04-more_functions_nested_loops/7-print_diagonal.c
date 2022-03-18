0b0c06e1330f16ea9e47354e1c2ff93a72702b7c
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-17 17:14:11 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print '\' n times
+ * @n: length of line
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+	}
+}
