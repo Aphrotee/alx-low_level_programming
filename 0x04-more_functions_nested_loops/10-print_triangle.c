c0a9b27ab0177909a3f403f759d6cfe3d614e89f
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-17 18:36:43 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle of size height and base
+ * @size: length of height and base
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+	if (size > 0)
+	{
+		for (i = size; i > 0; i--)
+		{
+			for (j = i - 1; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < (size - (i - 1)); k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}	
