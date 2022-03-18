e7455a35177d82d837dd82ce2fda123a89964114
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-17 20:37:36 -0700

    task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+int main()
+{
+	long long int i, j, k, n = 612852475143;
+
+	i = 2;
+	while(i < n)
+	{
+		if (n % i == 0)
+		{
+			j = 2;
+			while(j < i)
+			{
+				if (i % j == 0)
+				{
+					break;
+				}
+				else
+					j++;
+			}
+			if (j == i)
+				k = i;
+		}
+		i++;
+	}
+	printf("%lld\n", k);
+	return (0);
+}
