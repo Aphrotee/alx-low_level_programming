c4e5844cc268d3947f08a09cdf1503a12ef43cd0
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-18 09:08:11 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loops
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\0/\n");
+
+	return (0);
+}
