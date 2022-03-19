e6a999d3f306ea073002cb51a14b85ba59604a8e
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-18 19:16:58 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints first 50 terms of fibonacci sequence
+ * main - prints first 98 terms of fibonacci sequence
  *
  * Return: 0
  */
 int main(void)
 {
-	long a, b, c, d;
+	unsigned long a, b, c, d;
 
 	b = 1;
 	c = 2;
-	printf("%ld, %ld", b, c);
-	for (a = 0; a < 48; a++)
+	printf("%lu, %lu", b, c);
+	for (a = 0; a < 96; a++)
 	{
 		d = b + c;
 		b = c;
 		c = d;
-		printf(", %ld", d);
+		if (d < 0)
+			d *= -1;
+		printf(", %lu", d);
 	}
 	printf("\n");
 	return (0);
