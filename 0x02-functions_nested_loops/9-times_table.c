ea3cd1b00be28b193be0b2ec4e2e9c9e7d7bd83a
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-03-19 01:54:13 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,18 +5,61 @@
  *
  * Return: void
  */
+/**
+ * print_number - prints number up to n times
+ * @n: integer to print
+ 
+ * Return: void
+ */
+void print_number(int n)
+{
+	int a, b;
+
+	if (n != 0)
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		a = n / 10;
+		b = n % 10;
+		if (a != 0)
+		{
+			print_number(a);
+			_putchar(b + '0');
+		}
+		else
+			_putchar(b + '0');
+
+	}
+	else
+		_putchar('0');
+}
+
 void times_table(void)
 {
 	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
 		{
 			k = i * j;
-			_putchar(k + '0');
-			_putchar(',');
-			_putchar(' ');
+			if ((k > 9) && (k < 100))
+			{
+				_putchar(',');
+				_putchar(' ');
+				print_number(k);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
 		}
 		_putchar('\n');
 	}
