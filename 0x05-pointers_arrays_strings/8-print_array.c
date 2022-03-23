5c5d13ca1a3ae28d43a704b12f10590115247f63
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-22 17:26:07 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints an array
+ * @a: array pointer
+ * @n: index array to be printed up to
+ * 
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i, j, k;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", *a);
+		a++;
+	}
+	printf("%d\n", *a);
+}
