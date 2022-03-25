086b21a50c28b7454628b4ace8b625856c587d36
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-25 08:58:20 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string input
+ *
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	char *d;
+
+	d = s;
+	while (*s != '\0')
+	{
+		if ((*s == 'A') || (*s == 'a'))
+			*s = '4';
+		else if ((*s == 'E') || (*s == 'e'))
+			*s = '3';
+		else if ((*s == 'O') || (*s == 'o'))
+			*s = '0';
+		else if ((*s == 'T') || (*s == 't'))
+			*s = '7';
+		else if ((*s == 'L') || (*s == 't'))
+			*s = '1';
+		s++;
+	}
+	return (d);
+}
