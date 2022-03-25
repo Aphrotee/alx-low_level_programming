8418fb0c8e7ddeedfee02cc679fd536cbb057e52
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-25 09:33:13 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot 13
+ * @s: input 13
+ *
+ * Return: pointer to s
+ */
+char *rot13(char *s)
+{
+	char *d;
+	int j;
+
+	d = s;
+	j = 0;
+	while (*s != '\0')
+	{
+		if (((*s >= 'a') && (*s <= 'm')) || ((*s >= 'A') && (*s <= 'M')))
+		{
+			*s += 13;
+		}
+		s++;
+		j++;
+	}
+	while (j >= 0)
+	{
+		if (((*s >= 'N') && (*s <= 'Z')) || ((*s >= 'n') && (*s <= 'z')))
+		{
+			*s -= 13;
+		}
+		s--;
+		j--;
+	}
+	return (d);
+}
