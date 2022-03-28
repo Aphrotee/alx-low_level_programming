449251833a7f88646ce168c72409489370864a7a
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-28 15:56:33 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** _strchr - checks for a particular character in a string
+ * @s: string to be checked
+ * @c: character to look for
+ *
+ * Return: pointer to charcter if found
+ */
+char *_strchr(char *s, char c)
+{
+	char *d;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			d = s;
+			break;
+		}
+		s++;
+	}
+	return (d);
+}
