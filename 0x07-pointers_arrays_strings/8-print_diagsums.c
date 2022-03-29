a80c9996557d9990a73d2ae7d443b471726d10cf
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-03-29 01:52:59 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - prints the sums of the elements along both diagonals
@@ -12,11 +13,11 @@ void print_diagsums(int *a, int size)
 	int n, i, j, c, b;
 
 	n = size;
-	c = 0;
-	b = 0;
-	for (i = 0; i < n; i++)
+	c = *(a + (n - 1));
+	b = *a;
+	for (i = 0; i < n - 1; i++)
 	{
-		b += a[i][i];
+		b += *(b + (n + 1));
 	}
 	for (j = n - 1; i = 0; j >= 0; j--; i++)
 	{
