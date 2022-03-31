e2f5a9cb6fea965b7fdab83e806dfcebe81cec9b
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-03-30 17:46:40 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises x to the power of y
+ * @x: base number integer
+ * @y: power integer
+ *
+ * Return: value of x ** y
+ */
+int _pow_recursion(int x, int y)
+{
+	int b;
+
+	if (y == 0)
+		b = 1;
+	else if (b < 0)
+		b = -1;
+	else 
+	{
+		y--;
+		b = _pow_recursion(x, y);
+		b *= x;
+	}
+	return (b);
+}
