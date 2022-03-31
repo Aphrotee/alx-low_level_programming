4e2d3bda14781fc5477730c3e7b44850629adc79
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-03-31 18:25:54 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,9 +6,52 @@
  *
  * Return: squareroot of n
  */
-trial_guess(int a)
+
+/**
+ * try_again - returns the squareroot of a number
+ * @n: number to be rooted
+ * @i: dividend
+ *
+ * Return: squareroot of n
+ */
+
+int try_again(int n, int i)
 {
+	i++;
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (n > 1)
+	{
+		if ( i * i == n)
+			return (i);
+		else
+		{
+			i = try_again(n, i);
+		}	return(i);
+	}
 }
 int _sqrt_recursion(int n)
 {
+	int b = 1;
+
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (n > 1)
+	{
+		if ( b * b == n)
+			return (b);
+		else
+		{
+			b = try_again(n, b);
+			return(b);
+		}
+	}
 }
