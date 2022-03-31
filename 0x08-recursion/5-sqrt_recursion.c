efc02b2627599037050bf4f72142160670829b7f
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-03-31 19:08:46 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _try_again(int n, int i);
 
 /**
  * _sqrt_recursion - returns the squareroot of a number
@@ -7,36 +8,6 @@
  * Return: squareroot of n
  */
 
-/**
- * _try_again - returns the squareroot of a number
- * @n: number to be rooted
- * @i: dividend
- *
- * Return: squareroot of n
- */
-
-int _try_again(int n, int i)
-{
-	int j;
-
-	i++;
-	if (n < 0)
-		j = -1;
-	if (n == 0)
-		j = 0;
-	if (n == 1)
-		j = 1;
-	if (n > 1)
-	{
-		if (i * i == n)
-			j = i;
-		else
-		{
-			i = _try_again(n, i);
-		}	j = i;
-	}
-	return (j);
-}
 int _sqrt_recursion(int n)
 {
 	int b = 1, d;
@@ -58,4 +29,33 @@ int _sqrt_recursion(int n)
 		}
 	}
 	return (d);
+}
+/**
+ * _try_again - returns the squareroot of a number
+ * @n: number to be rooted
+ * @i: dividend
+ *
+ * Return: squareroot of n
+ */
+int _try_again(int n, int i)
+{
+	int j;
+
+	i++;
+	if (n < 0)
+		j = -1;
+	if (n == 0)
+		j = 0;
+	if (n == 1)
+		j = 1;
+	if (n > 1)
+	{
+		if (i * i == n)
+			j = i;
+		else
+		{
+			i = _try_again(n, i);
+		}	j = i;
+	}
+	return (j);
 }
