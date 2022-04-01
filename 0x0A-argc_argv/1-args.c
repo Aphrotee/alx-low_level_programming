e2201ccf142dcb4bb703afa4cb3c425b2a61c9c6
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-01 15:28:52 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: size of command array
+ * @argv: command array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
