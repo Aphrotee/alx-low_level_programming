bf46901d5a167b45e22987332848b604efcfedf1
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-01 10:59:13 -0700

    task 0

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - checks for a string in another string
+ * @needle: string to be checked for
+ * @haystack: string to be checked
+ *
+ * Return: pointer to haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (haystack);
+}
