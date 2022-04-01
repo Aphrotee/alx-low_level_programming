d6afdba4b479534f722c1371edb2aa1f66e8ff0a
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-01 15:19:40 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: size of command array
+ * @argv: command array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
