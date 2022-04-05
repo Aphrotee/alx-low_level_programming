06c1cee8dd0ca0ed17d09f6194289b520e63228e
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-05 07:16:02 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string and returns ptr to duplicate
+ * @str: string to be duplicated
+ *
+ * Return: pointer to duplicate
+ */
+char *_strdup(char *str)
+{
+	char *d, *nstr;
+
+	if (*str == '\0')
+		d = NULL;
+	else
+	{
+		nstr = malloc(sizeof(str));
+		d = nstr;
+		while (*Str != '\0')
+		{
+			*nstr = *str;
+			str++;
+			nstr++;
+		}
+	}
+	return (d);
+}
