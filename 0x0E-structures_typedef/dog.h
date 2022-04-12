fc72d1ad062685ac85e1921e248eb8ba0bde66c1
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-12 13:40:27 -0700

    readme and task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - details about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
