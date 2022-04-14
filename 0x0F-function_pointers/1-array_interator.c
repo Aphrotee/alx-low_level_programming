822ff6b26af6656a8bb3d85deced82e5b614b59e
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-14 09:46:43 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_interator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - performs the function pointed by
+ * action on the array elemnts
+ * @array: array containing the elments to be worked on
+ * @size: size of array
+ * @action: pointer to function to be performed
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (action == NULL)
+		return;
+	int i;
+
+	for (i = 0; i < size; i++;)
+		action(array[i]);
+}
