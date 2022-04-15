1b00fb8f0b6816b9c74d5a3fc612c4ad2a190058
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-04-15 01:38:52 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -10,3 +10,47 @@ int op_add(int a, int b)
 {
 	return(a + b);
 }
+/**
+ * op_sub - subtracts b from a
+ * @a: first int
+ * @b: second int
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+/**
+ * op_mul - multiplies a and b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+/**
+ * op_div - divides a by b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+/**
+ * op_mod - runs mod of a and b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return(a % b);
+}
