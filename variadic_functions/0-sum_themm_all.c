7b7417937176dc8bfd6d9103f4b75c04636dae68
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-16 10:45:27 -0700

    task 0

---
+++ variadic_functions/0-sum_themm_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all the arguments passed in from the second argument
+ * @n: number of arguments to be added
+ *
+ * Return: sum of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, d;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		d += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (d);
+}
