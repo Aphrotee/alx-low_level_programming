458d57dcf8a9ba9663ac64ad711440075bbb16aa
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-16 16:51:51 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints integer arguments from the third argument
+ * @separator: character(s) to be used to separate integers
+ * @n: number or arguments
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i; 
+
+	if ((separator == NULL) || (n == 0))
+		return;
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
