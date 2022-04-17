2191c1ebb95bfa92d63af9f7efa69e44063c4051
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-04-17 01:05:02 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i != n - 1)
+		if ((i != n - 1) && (separator != NULL))
 			printf("%s", separator);
 	}
 	va_end(ap);
