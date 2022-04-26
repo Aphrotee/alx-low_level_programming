46aeef185570a2adb467a10c388343d1aee53b59
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-04-27 00:39:57 +0100

    Create lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+extern size_t print_list(const list_t *h);
+extern int _putchar(char c);
+#endif
