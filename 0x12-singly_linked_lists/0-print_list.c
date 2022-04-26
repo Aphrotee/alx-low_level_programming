4a683cf26860f6fc6dd9c90e598fbd74c0e723e1
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-26 10:05:56 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * print_list - prints the content of the node which is a string
+ * @h: pointer to struct node
+ *
+ * Return: number of nodes or length of list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i, n;
+	size_t d = 0;
+	char *s;
+	const list_t *ptr = NULL;
+
+	ptr = h;
+	if (ptr == NULL)
+	{
+		return (d);
+	}
+	while (ptr != NULL)
+	{
+		n = ptr->len;
+		s = ptr->str;
+		if (s == NULL)
+			n = 0;
+		_putchar('[');
+		_putchar(n + '0');
+		_putchar(']');
+		_putchar(' ');
+		if (s == NULL)
+		{
+			_putchar('(');
+			_putchar('n');
+			_putchar('i');
+			_putchar('l');
+			_putchar(')');
+		}
+		else
+		{
+			for (i = 0; i < n; i++)
+			{
+				_putchar(*s);
+				s++;
+			}
+		}
+		_putchar('\n');
+		ptr = ptr->next;
+		d++;
+	}
+	return(d);
+}
