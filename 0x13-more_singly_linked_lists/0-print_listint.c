e6bbf9455ea5641db77d604a9690d86b9c1b1b89
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-04-29 08:23:35 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @head: listint_t list
+ *
+ * Return: number of elements in the list
+ */
+size_t print_listint(const listint_t *head)
+{
+	const listint_t *temp = (listint_t *)malloc(sizeof(listint_t));
+	size_t d = 0;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		d++;
+	}
+	return (d);
+}
