a2096a163f818a3df6529f5ede1508b48c3c9425
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-05-02 13:29:49 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void print_number(int n);
 
 /**
  * print_to_98 - prints all integers from n to 98
@@ -14,7 +15,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i + '0');
+			print_number(i);
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -23,12 +24,45 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i + '0');
+			print_number(i);
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
 	else if (n == 98)
-		_putchar(n + '0');
+	{
+		_putchar('9');
+		_putchar('8');
+	}
 	_putchar('\n');
+}
+/**
+ * print_number - prints out input
+ * @n: input
+ * Return: void
+ */
+void print_number(int n)
+{
+	int a, b;
+
+	if (n != 0)
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		a = n / 10;
+		b = n % 10;
+		if (a != 0)
+		{
+			print_number(a);
+			_putchar(b + '0');
+		}
+		else
+			_putchar(b + '0');
+
+	}
+	else
+		_putchar('0');
 }
