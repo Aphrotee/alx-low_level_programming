246b2a9c0a67f60aedc6e29f229966a210eaed0d
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-05-05 16:36:06 +0100

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary of an integer
+ * @n: int to be convertsed to binary
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int a, b;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	a = n >> 1;
+	b = a << 1;
+	if (a != 0)
+		print_binary(a);
+	if (n - b == 1)
+		_putchar('1');
+	else if (n - b == 0)
+		_putchar('0');
+}
+
+
+/**
+ * print_binary - prints the binary of an integer
+ * @n: int to be convertsed to binary
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int a, b;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	a = n >> 1;
+	b = a << 1;
+	if (a != 0)
+		print_binary(a);
+	if (n - b == 1)
+		_putchar('1');
+	else if (n - b == 0)
+		_putchar('0');
+}
