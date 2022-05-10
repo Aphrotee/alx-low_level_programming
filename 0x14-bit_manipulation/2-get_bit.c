120952954fa0609770295e97c5b3a341387fa092
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-10 11:46:36 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * get_bit - prints the binary of an integer
@@ -9,28 +11,30 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int a, b, c, x, y, z;
+	unsigned long int a, b, x, y, z;
 	unsigned int i = 1, j = 1;
-	
+	int *arr, k;
 
 	a = n >> 1;
 	x = n >> 1;
+	b = x << 1;
 	while (a != 0)
 	{
-		c = a;
 		a = a >> 1;
-		b = a << 1;
 		j++;
 	}
 	if (index > j)
 		return (-1);
-	while (i < (index + 1))
+	arr = malloc(sizeof(int) * j);
+	arr[0] = (int)(n - b);
+	while (x != 0)
 	{
 		z = x;
 		x = x >> 1;
 		y = x << 1;
+		arr[i] = (int)(z - y);
 		i++;
 	}
-	k = (int)(z - y)
+	k = arr[index];
 	return (k);
 }
