612b15ec35cab39011b98230a355e3fde919808c
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-05-11 14:42:12 +0100

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * clear_bit - sets a bit of binary of an integer to 0
+ * @n: pointer to int whose bit is to be set to 0
+ * @index: index of bit wanted
+ *
+ * Return: integer
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a, b, x, y, z, p = *n;
+	unsigned int i = 1, j = 1, m, l;
+	int *arr, k = 0;
+
+	if (index > 100)
+		return (-1);
+	a = p >> 1;
+	x = p >> 1;
+	b = x << 1;
+	while (a != 0)
+	{
+		a = a >> 1;
+		j++;
+	}
+	if (index > j)
+		arr = malloc(sizeof(int) * (1 + index));
+	else
+		arr = malloc(sizeof(int) * j);
+	arr[0] = (int)(p - b);
+	while (x != 0)
+	{
+		z = x;
+		x = x >> 1;
+		y = x << 1;
+		arr[i] = (int)(z - y);
+		i++;
+	}
+	if (index > j)
+	{
+		for (l = i; l < index; l++)
+			arr[l] = 0;
+		l++;
+	}
+	else
+		l = i;
+	arr[index] = 0;
+	for (m = 0; m < l; m++)
+		k += arr[m] << m;
+	*n = k;
+	return (1);
+}
