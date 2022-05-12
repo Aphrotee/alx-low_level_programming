749495aa4d7d296a0c8579805bdeb7efad8b0dcb
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-05-12 18:07:27 +0100

    task   6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,14 @@
 /**
  * get_endianness - checks if machine is little of big endian
- * 
+ *
  * Return: 0 or 1
  */
 int get_endianness(void)
 {
 	unsigned int x = 0x76543210;
-  	char *c = (char*)&x;
+	char *c = (char *)&x;
 	int d;
- 
+
 	if (*c == 0x10)
 		d = 1;
 	else
