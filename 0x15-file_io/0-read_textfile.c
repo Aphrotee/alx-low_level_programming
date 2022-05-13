67b63b672631864ad298deff6ca94469da83e541
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-13 10:26:32 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,9 @@
-#include <fctl.h>
+#include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * read_textfile - reads the content of a text file
@@ -14,6 +18,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf = (char *)malloc(sizeof(char) * letters);
 	ssize_t n, d;
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	n = read(fd, buf, letters);
 	d = write(1, buf, n);
