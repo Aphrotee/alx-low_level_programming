50bd8dd8ae69fb6d99f3106d2af26d0b7f7e1777
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-14 09:54:49 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,8 +17,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, d;
 	ssize_t a;
 
+	if (filename == NULL)
+		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd != -1)
+	if (fd != -1) && (text_content != NULL)
 		a = write(fd, text_content, strlen(text_content));
 	if (a != -1)
 		d = 1;
