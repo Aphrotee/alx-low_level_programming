a2bf1b5a33df4135808a7fc0c4e72ceba689f65f
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-05-27 18:56:41 +0100

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees all memory allocated to listint_t
+ * @head: list listint_t
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp, *h = head;
+
+	if (h == NULL)
+		return;
+	while (h->next != NULL)
+	{
+		temp = h->next;
+		free(h);
+		h = temp;
+	}
+	free(h);
+	head = NULL;
+}
