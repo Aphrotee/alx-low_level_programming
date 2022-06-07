9bc7c5161727a3c7f01bcaf012d946df57deb2ed
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-07 05:26:14 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,18 +2,57 @@
 #include <stdlib.h>
 #include "lists.h"
 
+
+/**
+ * insert_node - inserts a node at particular index
+ * @new: node listint_t
+ * @idx: index
+ * @j: pointer
+ * @h: pointer
+ *
+ * Return: pointer
+ */
+listint_t *insert_node(listint_t *new, listint_t *j, listint_t *h, unsigned int idx)
+{
+	unsigned int i = 0;
+	while (h != NULL)
+		{
+			if (idx == 0)
+			{
+				new->next = h;
+				*head = new;
+				return (new);
+			}
+			else if (i == idx)
+			{
+				j->next = new;
+				new->next = h;
+				return (new);
+			}
+			else if (h->next == NULL)
+			{
+				h->next = new;
+				new->next = NULL;
+				return (new);
+			}
+			i++;
+			j = h;
+			h = h->next;
+		}
+	return (NULL);
+}
 /**
  * insert_nodeint_at_index - inserts a node at particular index
  * @head: list listint_t
  * @idx: index
  * @n: data n
  *
- * Return: inserted node
+ * Return: pointer
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *h, *j, *c, *new = (listint_t *)malloc(sizeof(listint_t));
-	unsigned int i = 0, x = 0;
+	listint_t *h, *d, *j, *c, *new = (listint_t *)malloc(sizeof(listint_t));
+	unsigned int x = 0;
 
 	new->n = n;
 	if (head == NULL)
@@ -36,29 +75,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx > x)
 		return (NULL);
-	while (h != NULL)
-	{
-		if (idx == 0)
-		{
-			new->next = h;
-			*head = new;
-			return (new);
-		}
-		else if (i == idx)
-		{
-			j->next = new;
-			new->next = h;
-			return (new);
-		}
-		else if (h->next == NULL)
-		{
-			h->next = new;
-			new->next = NULL;
-			return (new);
-		}
-		i++;
-		j = h;
-		h = h->next;
-	}
-	return (NULL);
+	d = insert_node(new, j, h, idx);
+	return (d);
 }
