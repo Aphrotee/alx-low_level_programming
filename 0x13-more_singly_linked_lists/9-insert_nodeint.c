e0685a01c24e18b33f6967a73a6c9ab0a2851df7
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-07 05:44:59 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -58,7 +58,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int x = 0;
 
 	new->n = n;
-	if (head == NULL)
+	if (head == NULL || new == NULL)
 		return (NULL);
 	if (*head == NULL && idx == 0)
 	{
