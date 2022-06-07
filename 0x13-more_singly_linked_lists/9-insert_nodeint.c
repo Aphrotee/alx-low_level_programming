31fdf54075517e4a48fab7a0b75e4b14ef5262c6
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-07 05:38:30 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -67,7 +67,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else if (*head == NULL && idx != 0)
 		return (NULL);
 	c = *head;
-	j = h;
+	j = *head;
 	while (c != NULL)
 	{
 		c = c->next;
