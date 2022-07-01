ff6aac98ff74dc8b00b682a19a3adf19bfba4f62
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-01 03:33:53 +0100

    handled error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_t;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
 	new_t = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (new_t == NULL)
 		return (NULL);
 	new_t->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
-	if (new_t->array)
+	if (new_t->array = NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new_t->size = size;
 	for (i = 0; i < size; i++)
 		new_t->array[i] = NULL;
