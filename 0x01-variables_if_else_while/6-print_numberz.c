52a17b44df55ffc51d79069fc26fd2a22484fcc6
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-27 02:32:20 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char ch = '0';
+	int ch;
 
-	for (ch = '0'; ch <= '9'; ch++)
-		putchar(ch);
+	for (ch = 0; ch < 10 ; ch++)
+		putchar(ch + '0');
 	putchar('\n');
 	return (0);
 }
