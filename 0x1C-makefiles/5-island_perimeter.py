20af16ba194526734fec2623e0d310e8b02c30c2
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-08-17 13:15:39 -0700

    task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""
+This module provides the function island_perimeter.
+"""
+
+
+def island_perimeter(grid):
+    """
+    This is a function that returns the perimeter
+    of the island described in grid.
+    """
+    if len(grid) == 0 or not isinstance(grid, list):
+        return 0
+    else:
+        for a in grid:
+            if len(a) == 0:
+                return 0
+            elif not isinstance(a, list):
+                return 0
+
+    n = 0
+    for unit in grid:
+        for cell in unit:
+            if cell == 1:
+                n += 2
+    if n:
+        n += 2
+    return n
