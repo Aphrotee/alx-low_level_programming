63e1a89d6159006ce9c9a7fbccec73fe87cdde66
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-26 16:00:07 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,8 +13,8 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-extern void init_dog(struct dog *d, char *name, float age, char *owner);
-extern void print_dog(struct dog *d);
-extern dog_t *new_dog(char *name, float age, char *owner);
-extern void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
