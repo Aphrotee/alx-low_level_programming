aac3fb05946fd7caa7c7adef1c7d58334542df6a
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-26 16:15:32 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,8 +19,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 		printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
