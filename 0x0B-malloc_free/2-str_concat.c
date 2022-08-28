2f299760b84f1303722d63746463d701ba78c411
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-29 00:01:21 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,35 +1,39 @@
-#include  "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
- * @s1: first string input
- * @s2: second string input
- *
- * Return: pointer to new string
+ * str_concat - concatenate two strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concat string
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *d, *nstr = malloc(sizeof(s1) + sizeof(s2) - 4);
+	char *a;
+	int i, j, c, d;
 
-	d = nstr;
-	if (!nstr)
-		return (NULL);
 	if (s1 == NULL)
-		s1 = "\0";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = "\0";
-	while (*s1 != '\0')
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		*nstr = *s1;
-		nstr++;
-		s1++;
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
 	}
-	while (*s2 != '\0')
-	{
-		*nstr = *s2;
-		nstr++;
-		s2++;
-	}
-	return (d);
+
+	return (a);
 }
