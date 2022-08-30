92cba8c1ef3da0767c952beb2b0f49fe32e2808f
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-08-29 17:00:55 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,20 @@
 #include <stdlib.h>
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 #include <stddef.h>
+
+/**
+ * free_everything - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
+void free_everything(char **string, int i)
+{
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
+}
 
 /**
  * strtow - splits a string into words
@@ -11,31 +24,60 @@
  */
 char **strtow(char *str)
 {
-	char **arp, **d;
+	char *a, *x, *y, **arp, **d;
+	int i = 0, j, n;
 
-	if (str == NULL || strlen(str) == 0)
+	if (!str)
 		return (NULL);
-	arp = (char **)malloc(sizeof(char *) * 1024);
-	if (arp == NULL)
-		return (NULL);
-	d = arp;
-	if (*str == ' ')
+	a = str;
+	while (*str != '\0')
 	{
-		while (*str == ' ' && str != NULL)
-			str++;
+		if ((*str != ' ' && *str != '\0') &&
+				(*(str + 1) == '\0' || *(str + 1) == ' '))
+			i++;
+		str++;
 	}
-	while (str != NULL)
-	{
-		if (*str != ' ')
+	str = a;
+	arp = (char **)malloc(sizeof(char *) * (i + 1));
+	if (!arp)
+		return (NULL);
+	arp[i - 1] = NULL;
+	d = arp;
+	for (j = 0; j < i; j++)
+	{	x = NULL;
+		y = NULL;
+		n = 0;
+		while (*str != '\0')
 		{
-			*arp = str;
-			arp++;
-			while (*str != ' ' && str != NULL)
-				str++;
-			while (*str == ' ' && str != NULL)
+			if (*str != ' ' && *str != '\0')
+			{
+				x = str;
+				while (*str != '\0' && *str != ' ')
+				{
+					n++;
+					str++;
+				}
+				y = str;
+				break;
+			}
+			else if (*str == ' ')
 				str++;
 		}
+		arp[j] = malloc(sizeof(char) *(n + 1));
+		if (!(arp[j]))
+		{
+			free_everything(arp, j);
+			return (NULL);
+		}
+		y = arp[j];
+		while (*x != ' ' && *x != '\0')
+		{
+			*(arp[j]) = *x;
+			x++;
+			(arp[j])++;
+		}
+		*(arp[j]) = '\0';
+		arp[j] = y;
 	}
-	*arp = NULL;
 	return (d);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,3 +6,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+void print_tab(char **tab);
