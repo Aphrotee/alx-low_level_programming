df04e6a2da18f939e1c038b0771b0c01588ef379
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-31 02:33:25 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,52 +4,23 @@
 #include <stdio.h>
 
 /**
- * print_list - prints the content of the node which is a string
- * @h: pointer to struct node
- *
- * Return: number of nodes or length of list
+ * print_list - print all elements of `list_t` list
+ * @h: head of linked list
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int i, n;
-	size_t d = 0;
-	char *s;
-	const list_t *ptr = NULL;
+	const list_t *current;
+	size_t c;
 
-	ptr = h;
-	if (ptr == NULL)
+	c = 0;
+	current = h;
+	while (current != NULL)
 	{
-		return (d);
+		printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		c++;
 	}
-	while (ptr != NULL)
-	{
-		n = ptr->len;
-		s = ptr->str;
-		if (s == NULL)
-			n = 0;
-		_putchar('[');
-		_putchar(n + '0');
-		_putchar(']');
-		_putchar(' ');	
-		if (s == NULL)
-		{
-			_putchar('(');
-			_putchar('n');
-			_putchar('i');
-			_putchar('l');
-			_putchar(')');
-		}
-		else
-		{
-			for (i = 0; i < n; i++)
-			{
-				_putchar(*s);
-				s++;
-			}
-		}
-		_putchar('\n');
-		ptr = ptr->next;
-		d++;
-	}
-	return (d);
+
+	return (c);
 }
