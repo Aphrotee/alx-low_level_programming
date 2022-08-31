093c998f9ab81e8fd34ca89578e1bb6146998513
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-31 02:58:04 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,43 @@
-#include <string.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-
+#include <string.h>
+#include <stdio.h>
 /**
- * add_node_end - adds a node to the end of a linked list
- * @head: address of pointer to linked list
- * @str: string to be added
- *
- * Return: pointer to list
+ * add_node_end - add a new node at the end of `list_t` list
+ * @head: double pointer to head
+ * @str: string to duplicate into new node
+ * Return: Address of the new element or NULL if failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *ptr = NULL, *temp;
+	list_t *current;
+	list_t *new_node;
+	int c;
 
-	temp = *head;
-	ptr = (list_t *)malloc(sizeof(list_t));
-	if (!ptr)
+	current = *head;
+	while (current && current->next != NULL)
+		current = current->next;
+
+	for (c = 0; dup_str[c] != '\0'; c++)
+		;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 	{
-		free(ptr);
+		free(new_node);
 		return (NULL);
 	}
-	ptr->str = strdup(str);
-	if (!(ptr->str))
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		free(ptr);
+		free(new_node);
 		return (NULL);
 	}
-	ptr->next = NULL;
-	ptr->len = strlen(str);
-	if (*head == NULL)
-		*head = ptr;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = ptr;
-	return (ptr);
+	new_node->len = c;
+	new_node->next = NULL;
+
+	if (current)
+		current->next = new_node;
+	else
+		*head = new_node;
+	return (new_node);
 }
