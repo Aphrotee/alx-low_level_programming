f90ed01fc9bd5aace0e2590ca7cf9ab227fd2457
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-31 02:36:09 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,9 +11,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 extern size_t print_list(const list_t *h);
 extern int _putchar(char c);
