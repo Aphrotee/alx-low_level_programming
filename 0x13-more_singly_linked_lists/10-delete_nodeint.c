c24f9509c1e7bdd8f10629a138b775e5278613d7
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-08-30 17:21:42 -0700

    tasks 10 & 11

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *  delete_nodeint_at_index - This is  a function that deletes the
+ * node at index index of a listint_t linked list.
+ * @head: head node of linked list.
+ * @index: index of node to be deleted.
+ *
+ * Return: 1 or -1.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *temp2;
+	int i = 0;
+
+	if (!head || !(*head))
+		return (1);
+	temp = *head;
+	temp2 = temp->next;
+	if (index == 0)
+	{
+		free(*head);
+		*head = temp2;
+	}
+	while (temp2)
+	{
+		i++;
+		if (i == index)
+		{
+			temp->next = temp2->next;
+			free(temp2);
+			return (1);
+		}
+		temp = temp2;
+		temp2 = temp2->next;
+	}
+	return (-1);
+}
---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - This is a function that reverses a listint_t linked list.
+ * @head: address of pointer to the head node of the linked list.
+ *
+ * Return: head node.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp, *temp2;
+
+	if (!head || !(*head))
+		return (NULL);
+	temp = *head;
+	temp = temp->next;
+	temp2 = temp->next;
+	(*head)->next = NULL;
+	while (temp2)
+	{
+		temp->next = *head;
+		*head = temp;
+		temp = temp2;
+		temp2 = temp2->next;
+	}
+	temp->next = *head;
+	*head = temp;
+	return (*head);
+}
