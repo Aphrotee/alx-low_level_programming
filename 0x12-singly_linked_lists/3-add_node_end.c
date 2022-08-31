05aeceb5748b77121434b5154d78a884d510c1bf
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-31 02:43:44 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *ptr = NULL, *temp;
 
-	temp->next = *head;
+	temp = *head;
 	ptr = (list_t *)malloc(sizeof(list_t));
 	if (!ptr)
 		return (NULL);
