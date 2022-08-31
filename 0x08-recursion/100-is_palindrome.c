c3f121fc84e271fd112f0207250fd727832e9f94
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-08-31 04:06:08 +0100

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * iss_palindrome - This is  a function that returns 1 if a
+ * string is a palindrome and 0 if not.
+ * @s: string to be checked.
+ * @i: current index of string being checked.
+ *
+ * Return: 0 or 1
+ */
+int iss_palindrome(char *s, int i)
+{
+	int ssize;
+
+	ssize = strlen(s);
+	ssize--;
+
+	if (i > ssize / 2)
+		return (1);
+	if (s[i] == s[ssize - i])
+		return (iss_palindrome(s, i + 1));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - This is  a function that returns 1 if a
+ * string is a palindrome and 0 if not.
+ * @s: string to be checked.
+ *
+ * Return: 0 or 1
+ */
+int is_palindrome(char *s)
+{
+	int i;
+	i = 0;
+
+	return (iss_palindrome(s, i));
+}
